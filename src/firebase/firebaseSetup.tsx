05354ca8fe7543d6f4f92641de4026ad8d45ee6f
Russell McClellan <russell.mcclellan@gmail.com>
2023-08-27 22:26:52 -0400

    Add database to setup

--- src/firebase/firebaseSetup.tsx
+++ src/firebase/firebaseSetup.tsx
@@ -1,48 +1,60 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, signOut as firebaseSignOut } from "firebase/auth";
-import { getFunctions, httpsCallable , connectFunctionsEmulator } from 'firebase/functions';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut as firebaseSignOut,
+} from "firebase/auth";
+import {
+  getFunctions,
+  httpsCallable,
+  connectFunctionsEmulator,
+} from "firebase/functions";
 import { firebaseConfig } from "./creds";
+import { getDatabase } from "firebase/database";
 
 const firebaseApp = initializeApp(firebaseConfig);
 
 export const auth = getAuth(firebaseApp);
 export const functions = getFunctions(firebaseApp);
-//connectFunctionsEmulator(functions, "localhost", 5001);
+connectFunctionsEmulator(functions, "localhost", 5001);
 
-export const getImage = httpsCallable(functions, 'getImage');
-export const getVariant = httpsCallable(functions, 'getVariant');
+export const getImage = httpsCallable(functions, "getImage");
+export const getVariant = httpsCallable(functions, "getVariant");
 
 export const signIn = (email: string, password: string) => {
-    return signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            console.log('signed in as', userCredential.user);
-            const user = userCredential.user;
-            return user;
-            // ...
-        })
-        .catch((error) => {
+  return signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      console.log("signed in as", userCredential.user);
+      const user = userCredential.user;
+      return user;
+      // ...
+    })
+    .catch((error) => {
+      return error;
+    });
+};
 
-            return error;
-        });
-
-}
+export const database = getDatabase(firebaseApp);
 
 export const signOut = async () => {
-    await firebaseSignOut(auth);
+  await firebaseSignOut(auth);
 };
 
 export type ProtectedRouteProps = {
-    isAuthenticated: boolean;
-    redirect: JSX.Element;
-    outlet: JSX.Element;
+  isAuthenticated: boolean;
+  redirect: JSX.Element;
+  outlet: JSX.Element;
 };
 
-export const ProtectedRoute = ({ isAuthenticated, redirect, outlet }: ProtectedRouteProps) => {
-    if (isAuthenticated) {
-        return outlet;
-    } else {
-        return redirect;
-    }
+export const ProtectedRoute = ({
+  isAuthenticated,
+  redirect,
+  outlet,
+}: ProtectedRouteProps) => {
+  if (isAuthenticated) {
+    return outlet;
+  } else {
+    return redirect;
+  }
 };
-
